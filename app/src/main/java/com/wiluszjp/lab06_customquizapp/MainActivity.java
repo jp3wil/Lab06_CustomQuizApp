2c43cd09d8f0b1ca86c4cfba64de946a1735237d
jp3wil <jp32.wilusz@gmail.com>
2020-04-11 20:39:26 -0400

    Lab 06 Final

--- app/src/main/java/com/wiluszjp/lab06_customquizapp/MainActivity.java
+++ app/src/main/java/com/wiluszjp/lab06_customquizapp/MainActivity.java
@@ -21,12 +21,20 @@ public class MainActivity extends AppCompatActivity {
     RadioButton ansOne, ansTwo, ansThree, ansFour, ansYes, ansNo;
     Button proceedButton;
     SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     Resources res;
+    Gson gson;
     String[] responses;
+    String prevResp;
     int questionNo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        res = getResources();
+        gson = new Gson();
+
         setContentView(R.layout.activity_main);
         header = findViewById(R.id.header);
         previous = findViewById(R.id.prevResps);
@@ -34,28 +42,104 @@ public class MainActivity extends AppCompatActivity {
         writeAns = findViewById(R.id.writeResponse);
         fourAns = findViewById(R.id.fourResponse);
         ansOne = findViewById(R.id.check1);
+        ansOne.setText(res.getStringArray(R.array.four_check_layout)[0]);
         ansTwo = findViewById(R.id.check2);
+        ansTwo.setText(res.getStringArray(R.array.four_check_layout)[1]);
         ansThree = findViewById(R.id.check3);
+        ansThree.setText(res.getStringArray(R.array.four_check_layout)[2]);
         ansFour = findViewById(R.id.check4);
+        ansFour.setText(res.getStringArray(R.array.four_check_layout)[3]);
         twoAns = findViewById(R.id.twoResponse);
         ansYes = findViewById(R.id.trueResp);
+        ansYes.setText(res.getStringArray(R.array.true_false_layout)[0]);
         ansNo = findViewById(R.id.falseResp);
+        ansNo.setText(res.getStringArray(R.array.true_false_layout)[1]);
         proceedButton = findViewById(R.id.proceedButton);
-        sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE);
-        res = getResources();
 
+        responses = new String[3];
         questionNo = 0;
-        header.setText(res.getString(R.string.app_name));
-        previous.setText(sharedPreferences.getString("previousResponsesGSON", ""));
-        question.setText(res.getQuantityText(R.plurals.headerForm, 0));
+        header.setText(res.getQuantityString(R.plurals.headerForm, 0));
+        prevResp = sharedPreferences.getString("previousResponsesGSON", "");
+        if(!prevResp.equals("")) {
+            responses = gson.fromJson(prevResp, String[].class);
+            prevResp = "";
+            for(int i = 0; i < 3; i++) {
+                prevResp += responses[i];
+                if(i < 2)
+                    prevResp += "\n";
+            }
+        }
+        else {
+            prevResp = "";
+        }
+        previous.setText(prevResp);
+        question.setText(res.getString(R.string.question_null));
 
         proceedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(questionNo > 3) {
-                    question.setText(res.getQuantityText(R.plurals.headerForm, 0));
-
+                writeAns.setVisibility(View.GONE);
+                fourAns.setVisibility(View.GONE);
+                ansOne.setVisibility(View.GONE);
+                ansTwo.setVisibility(View.GONE);
+                ansThree.setVisibility(View.GONE);
+                ansFour.setVisibility(View.GONE);
+                twoAns.setVisibility(View.GONE);
+                ansYes.setVisibility(View.GONE);
+                ansNo.setVisibility(View.GONE);
+                if(questionNo < 3) {
+                    header.setText(res.getQuantityString(R.plurals.headerForm,1, questionNo + 1));
+                    question.setText(res.getStringArray(R.array.questions)[questionNo]);
+                    if(questionNo == 0) {
+                        writeAns.setVisibility(View.VISIBLE);
+                    }
+                    if(questionNo == 1) {
+                        responses[0] = (writeAns.getText()).toString();
+                        fourAns.setVisibility(View.VISIBLE);
+                        ansOne.setVisibility(View.VISIBLE);
+                        ansTwo.setVisibility(View.VISIBLE);
+                        ansThree.setVisibility(View.VISIBLE);
+                        ansFour.setVisibility(View.VISIBLE);
+                    }
+                    if(questionNo == 2) {
+                        RadioButton resp_four = findViewById(fourAns.getCheckedRadioButtonId());
+                        responses[1] = (resp_four.getText()).toString();
+                        twoAns.setVisibility(View.VISIBLE);
+                        ansYes.setVisibility(View.VISIBLE);
+                        ansNo.setVisibility(View.VISIBLE);
+                        proceedButton.setText(R.string.proceedButtOptTwo);
+                    }
                 }
+                else if(questionNo == 3) {
+                    header.setText(res.getQuantityString(R.plurals.headerForm,2));
+                    question.setText(res.getString(R.string.question_full));
+                    RadioButton resp_two = findViewById(twoAns.getCheckedRadioButtonId());
+                    responses[2] = (resp_two.getText()).toString();
+                    editor.putString("previousResponsesGSON", gson.toJson(responses));
+                    editor.apply();
+                    proceedButton.setText(R.string.proceedButtOptThree);
+                }
+                else {
+                    responses = new String[3];
+                    header.setText(res.getQuantityString(R.plurals.headerForm, 0));
+                    prevResp = sharedPreferences.getString("previousResponsesGSON", "");
+                    if(!prevResp.equals("")) {
+                        responses = gson.fromJson(prevResp, String[].class);
+                        prevResp = "";
+                        for(int i = 0; i < 3; i++) {
+                            prevResp += responses[i];
+                            if(i < 2)
+                                prevResp += "\n";
+                        }
+                    }
+                    else {
+                        prevResp = "";
+                    }
+                    previous.setText(prevResp);
+                    question.setText(res.getQuantityText(R.plurals.headerForm, 0));
+                    questionNo = -1;
+                }
+                questionNo++;
             }
         });
     }
